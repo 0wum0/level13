20c65a1133a2ffc573700ccc3d5997dd10b6a7e2
Florian Engelhardt <florian0engelhardt@gmail.com>
2026-07-14 08:59:07 +0200

    Release world migration startup fix

--- src/config.js
+++ src/config.js
@@ -18,7 +18,7 @@ require.config({
 
     config: {
         'level13-app': {
-            version: '0.7.2-sublevel',
+            version: '0.7.3-sublevel',
             isDebugVersion: false,
             isCheatsEnabled: false,
             isDebugOutputEnabled: false,
@@ -27,6 +27,6 @@ require.config({
         }
     },
 
-    urlArgs: 'v=0.7.2-sublevel'
+    urlArgs: 'v=0.7.3-sublevel'
 
 });
